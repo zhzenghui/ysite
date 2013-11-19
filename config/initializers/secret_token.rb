3c3c6c2aa445e42cb3d3e0aee2a7666477e1c9be
zhzenghui <zhzenghui@gmail.com>
2013-11-20 01:49:28 +0800

    crate mongoid project blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ysite::Application.config.secret_key_base = 'afa5f61f1c60c8747ad6f88f0793f595c741eb16b64a09e66504ab447f5b15645159de7a29cca7758d63a99d21144c16b985b761717b0d346fed6bd7b8e4ec89'
